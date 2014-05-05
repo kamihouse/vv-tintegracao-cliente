66bb933564f96468de91e3e94366b38267457e91
jefferson.sb.es@gmail.com <jefferson.sb.es@gmail.com@67b5ad9d-064c-1be1-fbe8-b1c806a26901>
2014-05-05 23:57:11 +0000

    Adicionado os pacotes de teste, dao, controle. Adicionado Classe GerenciadorCliente e interface CrudCliente

---
+++ vv-tintegracao-cliente/src/br/ufcqx/controle/GerenciadorCliente.java
@@ -0,0 +1,20 @@
+package br.ufcqx.controle;
+
+import java.util.ArrayList;
+import br.ufcqx.interfaces.CrudCliente;
+import br.ufcqx.modelo.Cliente;
+
+
+public class GerenciadorCliente implements CrudCliente{
+	private ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+
+	public void addCliente(Cliente cliente) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void deleteCliente(int cliente) {
+		// TODO Auto-generated method stub
+		
+	}
+}
