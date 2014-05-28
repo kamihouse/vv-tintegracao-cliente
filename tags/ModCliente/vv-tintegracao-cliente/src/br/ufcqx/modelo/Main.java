f4bb9fa7b59ed0d55a2a66db8db0402643ddbadb
kamihouse@gmail.com <kamihouse@gmail.com@67b5ad9d-064c-1be1-fbe8-b1c806a26901>
2014-05-28 14:19:09 +0000

    Tag ModCliente Versão 1.0

---
+++ tags/ModCliente/vv-tintegracao-cliente/src/br/ufcqx/modelo/Main.java
@@ -0,0 +1,22 @@
+package br.ufcqx.modelo;
+
+import java.sql.SQLException;
+
+import br.ufcqx.controle.GerenciadorCliente;
+
+public class Main {
+
+	public static void main(String[] args) throws SQLException {
+		Cliente cliente = new Cliente("Jo�o", "Rua dos testes", "Bairro dos Testes", 
+						"Cidade dos Testes", "Estado dos Testes", "63900-000", null, 
+						"123123", 213, "joao_teste@testes.com", 88880000, null);
+		
+		GerenciadorCliente gerCli = new GerenciadorCliente();
+		//gerCli.addCliente(cliente);	
+		//System.out.println("Delete Cliente: " + gerCli.deleteCliente(4));
+		
+		gerCli.addCliente(cliente);	
+		System.out.println("Delete Cliente: " + gerCli.deleteCliente(4));
+	}
+
+}
